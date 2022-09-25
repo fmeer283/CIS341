65da303e9d26b73f639761c5172386cc120d8795
My Name Would Go Here <myemail@example.com>
2022-09-25 16:41:24 -0500

    Added Contact Razor page and Model

---
+++ CIS341-Lab03/Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace CIS341_Lab03.Models
+{
+    public class Contact
+    {
+        string Name { get; set; }
+        string Email { get; set; }
+        string Message { get; set; }
+    }
+}
---
+++ CIS341-Lab03/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CIS341_Lab03.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
